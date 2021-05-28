Fix Mask-RCNN (added the ability to skip scaling to original resolution)

mmdet/models/roi_heads/mask_heads/fcn_mask_head.py
@@ -34,7 +34,8 @@ class FCNMaskHead(BaseModule):
                  norm_cfg=None,
                  loss_mask=dict(
                      type='CrossEntropyLoss', use_mask=True, loss_weight=1.0),
-                 init_cfg=None):
+                 init_cfg=None,
+                 rescale_mask_to_input_shape=True):
         assert init_cfg is None, 'To prevent abnormal initialization ' \
                                  'behavior, init_cfg is not allowed to be set'
         super(FCNMaskHead, self).__init__(init_cfg)
@@ -60,6 +61,7 @@ class FCNMaskHead(BaseModule):
         self.norm_cfg = norm_cfg
         self.fp16_enabled = False
         self.loss_mask = build_loss(loss_mask)
+        self.rescale_mask_to_input_shape = rescale_mask_to_input_shape
 
         self.convs = ModuleList()
         for i in range(self.num_convs):
@@ -261,8 +263,14 @@ class FCNMaskHead(BaseModule):
             if not self.class_agnostic:
                 box_inds = torch.arange(mask_pred.shape[0])
                 mask_pred = mask_pred[box_inds, labels][:, None]
-            masks, _ = _do_paste_mask(
-                mask_pred, bboxes, img_h, img_w, skip_empty=False)
+
+            #rescale_mask_to_input_shape = True
+            if self.rescale_mask_to_input_shape:
+                masks, _ = _do_paste_mask(
+                    mask_pred, bboxes, img_h, img_w, skip_empty=False)
+            else:
+                masks = mask_pred.squeeze()
+
             if threshold >= 0:
                 masks = (masks >= threshold).to(dtype=torch.bool)
             else: