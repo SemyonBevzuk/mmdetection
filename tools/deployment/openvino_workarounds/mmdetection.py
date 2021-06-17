00e1e6f3d2b2333fd600440482e8d7b81b08b595
Semyon Bevzyuk <semen.bevzuk@gmail.com>
2021-06-18 02:15:25 +0300

    Fix atss and foveabox metrics.

--- tools/deployment/openvino_workarounds/mmdetection.py
+++ tools/deployment/openvino_workarounds/mmdetection.py
@@ -1,6 +1,8 @@
 import importlib
 from functools import partial, wraps
 
+import torch
+
 
 def update_default_args_value(func, **updated_args):
     new_func = partial(func, **updated_args)
@@ -28,5 +30,30 @@ def fix_get_bboxes_output():
         head_class.get_bboxes = crop_output(head_class.get_bboxes)
 
 
+def fix_img_shape_type():
+    """Some models (ATSS, FoveaBox) use img_metas[0]['img_shape'], which type
+    is 'list'.
+
+    To export with dynamic inputs, the type should be changed to 'tensor'.
+    Can be fixed here: https://github.com/open-mmlab/mmdetection/pull/5251.
+    Or you can remove support for img_metas[0]['img_shape_for_onnx'] and
+    always convert values to a tensor with input dimension.
+    """
+
+    def rewrite_img_shape_in_onnx_export(function):
+
+        @wraps(function)
+        def wrapper(self, img, img_metas):
+            img_metas[0]['img_shape'] = torch._shape_as_tensor(img)[2:]
+            return function(self, img, img_metas)
+
+        return wrapper
+
+    from mmdet.models.detectors.single_stage import SingleStageDetector
+    SingleStageDetector.onnx_export = rewrite_img_shape_in_onnx_export(
+        SingleStageDetector.onnx_export)
+
+
 def apply_all_fixes():
     fix_get_bboxes_output()
+    fix_img_shape_type()
