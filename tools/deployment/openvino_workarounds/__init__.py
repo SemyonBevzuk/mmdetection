7e7322a05c01ec6f313b99330b5173853430ba41
Semyon Bevzyuk <semen.bevzuk@gmail.com>
2021-06-22 17:36:37 +0300

    Fix mkl-service + Intel(R) MKL: MKL_THREADING_LAYER=INTEL is incompatible with libgomp.so.1 library

--- tools/deployment/openvino_workarounds/__init__.py
+++ tools/deployment/openvino_workarounds/__init__.py
@@ -1,11 +1,4 @@
 import os
-
-from .symbolic import register_extra_symbolics_for_openvino
-from .mmdetection import update_default_args_value, \
-                         apply_all_fixes as apply_all_fixes_mmdetection
-from .openvino import rename_input_onnx, \
-                      apply_all_fixes as apply_all_fixes_openvino
-
 '''
 Error: mkl-service + Intel(R) MKL: MKL_THREADING_LAYER=INTEL
 is incompatible with libgomp.so.1 library.
@@ -13,6 +6,12 @@ is incompatible with libgomp.so.1 library.
     Set MKL_SERVICE_FORCE_INTEL to force it.
 '''
 os.environ['MKL_THREADING_LAYER'] = 'GNU'
+
+from .symbolic import register_extra_symbolics_for_openvino
+from .mmdetection import update_default_args_value, \
+                         apply_all_fixes as apply_all_fixes_mmdetection
+from .openvino import rename_input_onnx, \
+                      apply_all_fixes as apply_all_fixes_openvino
 
 __all__ = [
     'register_extra_symbolics_for_openvino',
